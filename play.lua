8e8b6da9dba8f0b1a4b083ac76ce0b4a932fcf97
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-08 13:55:30 -0500

    clicking play now starts the game

--- play.lua
+++ play.lua
@@ -1,15 +1,21 @@
 Play = {}
 
 function Play:load()
-
+	World:load()
+	Player:load()
+	Pipe:load()
 end
 
 
 function Play:update(dt)
-
+	World:update(dt)
+	Player:update(dt)
+	Pipe:update(dt)
 end
 
 
 function Play:draw()
-
+	Pipe:draw()
+	World:draw()
+	Player:draw()
 end
