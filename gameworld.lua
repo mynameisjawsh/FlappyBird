9c4da78ef9b11e3fbab8fb1dff2066ca2c12ce2d
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-08 09:28:24 -0500

    defined and drew visuals for game world

--- gameworld.lua
+++ gameworld.lua
@@ -1,7 +1,14 @@
 World = {}
 
 function World:load()
+	love.graphics.setBackgroundColor(0, 0.6, 1)
 
+	self.Ground = {
+		x = Screen.x,
+		y = Screen.height - 112,
+		width = Screen.width,
+		height = Screen.height
+	}
 end
 
 
@@ -11,5 +18,6 @@ end
 
 
 function World:draw()
-
+	love.graphics.setColor(0, 0.75, 0)
+	love.graphics.rectangle("fill", self.Ground.x, self.Ground.y, self.Ground.width, self.Ground.height)
 end
--- main.lua
+++ main.lua
@@ -1,5 +1,11 @@
 require("gameworld")
 
+Screen = {}
+Screen.x = 0
+Screen.y = 0
+Screen.width = love.graphics.getWidth()
+Screen.height = love.graphics.getHeight()
+
 function love.load()
 	World:load()
 end
