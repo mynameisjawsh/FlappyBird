c93807848e68a3d7c389e6fa69870dcbe46fab22
mynameisjawsh <joshkeckmusic@gmail.com>
2025-09-08 13:14:09 -0500

    defined and drew visuals for title screen

--- start.lua
+++ start.lua
@@ -1,7 +1,15 @@
 Start = {}
 
 function Start:load()
+	love.graphics.setBackgroundColor(0, 0.6, 1)
 
+	self.Title = {
+		font = love.graphics.newFont(52),
+		text = "Lua Bird",
+		x = Screen.width / 2,
+		y = 50,
+		xOffSet = 108
+	}
 end
 
 
@@ -11,5 +19,7 @@ end
 
 
 function Start:draw()
-
+	love.graphics.setColor(0, 0, 0)
+	love.graphics.setFont(self.Title.font)
+	love.graphics.print(self.Title.text, self.Title.x - self.Title.xOffSet, self.Title.y)
 end
